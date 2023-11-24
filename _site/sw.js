66d7cf0881a2ada8700aac30499473327563e9d4
Kim Gun Il <lurker18@naver.com>
2023-11-25 01:52:50 +0900

    update files

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700837748';
+const cacheName = 'chirpy-1700844740';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
